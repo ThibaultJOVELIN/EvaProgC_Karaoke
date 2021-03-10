f434d0d90f3b1ad0b16bbae0ce8cb789e41b7e84
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2021-03-10 10:18:09 +0100

    Update EvaKaraoke.cpp

--- EvaKaraoke.cpp
+++ EvaKaraoke.cpp
@@ -1,24 +1,6 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include "Player.h"
 using namespace std;
 
-class Player{
-
-    private: 
-
-        std::string NomJoueur;
-        int _score_obtenu;
-        int _score_précédent;
-        int _score;
-        int _meilleur_score;
-        int _pire_score;
-        int _moyenne;
-
-    public : 
-
-        void PireScore();
-        void MeilleurScore();
-        void Moyenne();
-        std::string EntrerNom();
-};

89c7cf570b5a11b305be6b7853da6b0db097d3bc
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2021-03-10 10:11:11 +0100

    Update EvaKaraoke.cpp

--- EvaKaraoke.cpp
+++ EvaKaraoke.cpp
@@ -2,3 +2,12 @@
 #include <vector>
 #include <iostream>
 using namespace std;
+
+class Player{
+
+    private: 
+
+        std::string NomJoueur;
+        int _score_obtenu;
+        int _score_précédent;
+

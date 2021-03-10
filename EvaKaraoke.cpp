b343cccdc9b7ed155257fea425abb257d26acab1
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2021-03-10 10:28:17 +0100

    Update EvaKaraoke.cpp

--- EvaKaraoke.cpp
+++ EvaKaraoke.cpp
@@ -4,3 +4,23 @@
 #include "Player.h"
 using namespace std;
 
+
+void Player::AjoutScore(){
+
+}
+
+void Player::PireScore(){
+
+}
+
+void Player::MeilleurScore(){
+    
+}
+
+void Player::Moyenne(){
+
+}
+
+std::string Player::EntrerNom(){
+
+}

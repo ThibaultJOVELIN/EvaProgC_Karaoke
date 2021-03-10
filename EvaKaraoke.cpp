72365b9d79bc7e0a3f47104ef79d5a9d0bd3febb
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2021-03-10 10:48:21 +0100

    Update EvaKaraoke.cpp

--- EvaKaraoke.cpp
+++ EvaKaraoke.cpp
@@ -9,12 +9,21 @@ void Player::AjoutScore(){
 
 }
 
+void Player::ChangementScore(){
+ if (_score_obtenu > _score_precedent)
+    _score == _score_obtenu;
+else
+    _score == _score_precedent;
+}
+
 void Player::PireScore(){
+    cout << _pire_score << endl;
 
 }
 
 void Player::MeilleurScore(){
-    
+    cout << _meilleur_score << endl;
+
 }
 
 void Player::Moyenne(){
@@ -22,5 +31,9 @@ void Player::Moyenne(){
 }
 
 std::string Player::EntrerNom(){
-
+    return NomJoueur;
 }
+
+
+
+

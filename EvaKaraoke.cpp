0d6269f7f1da9d4eb9460baa89546297f76ad789
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2021-03-10 11:10:42 +0100

    Update EvaKaraoke.cpp

--- EvaKaraoke.cpp
+++ EvaKaraoke.cpp
@@ -4,16 +4,31 @@
 #include "Player.h"
 using namespace std;
 
+char Musique [4];
 
 void Player::AjoutScore(){
 
 }
 
 void Player::ChangementScore(){
- if (_score_obtenu > _score_precedent)
-    _score == _score_obtenu;
-else
-    _score == _score_precedent;
+    if (_score_obtenu > _score_precedent)
+        _score == _score_obtenu;
+    else
+        _score == _score_precedent;
+}
+
+void Player::NombreMusique(){
+    if (_scoreM1 > 0)
+        _nombre_musique += 1;
+    if (_scoreM2 > 0)
+        _nombre_musique += 1;
+    if (_scoreM3 > 0)
+        _nombre_musique += 1;
+    if (_scoreM4 > 0)
+        _nombre_musique += 1;
+    if (_scoreM5 > 0)
+        _nombre_musique += 1;
+    
 }
 
 void Player::PireScore(){
@@ -27,6 +42,10 @@ void Player::MeilleurScore(){
 }
 
 void Player::Moyenne(){
+    if (_nombre_musique > 0)
+        _moyenne = _scoreM1 + _scoreM2 + _scoreM3 + _scoreM4 + _scoreM5;
+        _moyenne = _moyenne / _nombre_musique;
+        cout << _moyenne << endl;
 
 }
 
